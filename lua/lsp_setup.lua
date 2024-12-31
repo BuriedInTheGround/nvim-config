68224b793776ad27d4c5c17a0cfac192144487aa
Simone Ragusa <hi@interrato.dev>
2024-12-31 17:02:15 +0100

    lsp: include visual mode in code action keymap

--- lua/lsp_setup.lua
+++ lua/lsp_setup.lua
@@ -2,8 +2,9 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
   callback = function(event)
-    local map = function(keys, func, desc)
-      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
     -- Go to <thing>
@@ -23,7 +24,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Actions
     map('<Leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    map('<Leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('<Leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
     -- Workspace folders management
     map('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
